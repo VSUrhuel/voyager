2dede1edb0d11e66e8da0f9d1877894968892d61
John Rhuel Laurente <22-1-00109@vsu.edu.ph>
2025-04-01 23:50:13 +0800

    Integrate search functionality and refresh support
    Refactored code to enable searching within the mentee list. Ensured proper connection between search results and the displayed list. Finally, integrated refresh functionality to update results dynamically.

--- lib/src/features/mentor/screens/home/request_list.dart
+++ lib/src/features/mentor/screens/home/request_list.dart
@@ -1,8 +1,26 @@
-import 'package:voyager/src/features/mentee/widgets/normal_search_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:voyager/src/features/mentor/controller/mentee_list_controller.dart';
+import 'package:voyager/src/features/mentor/screens/home/pending.dart';
 
-class RequestList extends StatelessWidget {
+class RequestList extends StatefulWidget {
   const RequestList({super.key});
+
+  @override
+  State<RequestList> createState() => _RequestListState();
+}
+
+class _RequestListState extends State<RequestList> {
+  final MenteeListController menteeListController = MenteeListController();
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+      GlobalKey<RefreshIndicatorState>();
+  final GlobalKey _pendingListKey = GlobalKey();
+
+  Future<void> handleRefresh() async {
+    // Call the refresh method directly on the PendingList's state
+    if (_pendingListKey.currentState != null) {
+      (_pendingListKey.currentState as dynamic).refreshPendingMentees();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,44 +29,80 @@ class RequestList extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white, // Assuming a white background
-        elevation: 1.0, // Optional: Add a subtle shadow
+        backgroundColor: Colors.white,
+        elevation: 1.0,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.black), // Black arrow back
-          onPressed: () {
-            Navigator.pop(context); // Go back when pressed
-          },
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.pop(context),
         ),
-        title: Text(
+        title: const Text(
           'Pending Requests',
           style: TextStyle(
-            color: Colors.black, // Black text
-            fontWeight: FontWeight.normal, // Normal font weight
-            fontSize: 18.0, // Adjust font size as needed
+            color: Colors.black,
+            fontWeight: FontWeight.normal,
+            fontSize: 18.0,
           ),
         ),
-        centerTitle: true, // Align title to the left
+        centerTitle: true,
       ),
       body: Column(
         children: [
-          NormalSearchbar(), // Add the NormalSearchbar widget
-          Expanded(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: EdgeInsets.only(
-                    left: screenWidth * 0.05, right: screenWidth * 0.05),
-                child: Center(
-                  child: Column(children: [
-                    SizedBox(height: screenHeight * 0.02),
-                    Text(
-                      'No more requests',
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: screenWidth * 0.03,
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30.0),
+                border: Border.all(color: Colors.grey[300]!),
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: menteeListController.searchMenteeController,
+                      decoration: const InputDecoration(
+                        hintText: 'Search',
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(
+                          vertical: 15.0,
+                          horizontal: 7,
+                        ),
                       ),
+                      style: const TextStyle(fontSize: 16.0),
                     ),
-                    SizedBox(height: screenHeight * 0.02),
-                  ]),
+                  ),
+                  Icon(
+                    Icons.search,
+                    color: Colors.grey[600],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: RefreshIndicator(
+              key: _refreshIndicatorKey,
+              onRefresh: handleRefresh,
+              child: SingleChildScrollView(
+                physics: const AlwaysScrollableScrollPhysics(),
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    left: screenWidth * 0.05,
+                    right: screenWidth * 0.05,
+                  ),
+                  child: Center(
+                    child: Column(
+                      children: [
+                        SizedBox(height: screenHeight * 0.02),
+                        PendingList(
+                          menteeListController: menteeListController,
+                          key: _pendingListKey,
+                          isMentorHome: false,
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ),
             ),
